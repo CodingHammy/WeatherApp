6d1c4309fbdb0ded1a1fbedc3a4406df2a58efda
CodingHammy <Will.hamilton.job@gmail.com>
2025-02-19 11:18:06 +0000

    pushing starter files, first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className='text-3xl font-bold bg-amber-800'>will</div>;
+}
+
+export default App;
---
+++ tests/main.test.ts
@@ -0,0 +1,7 @@
+describe('Tester', () => {
+  const input = Math.sqrt(9);
+  it('should', () => {
+    expect(1 + 2).toEqual(3);
+    expect(input).toBe(3);
+  });
+});
